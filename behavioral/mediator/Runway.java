f7b038326cab64c0992c41dfb0b09d85be5b6a15
Saurabh Chaturvedi <56601821+saurabhchaturvedy@users.noreply.github.com>
2021-06-20 18:24:45 +0530

    Create Runway.java

---
+++ behavioral/mediator/Runway.java
@@ -0,0 +1,12 @@
+public class Runway {
+
+    private String name;
+
+    public Runway(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
